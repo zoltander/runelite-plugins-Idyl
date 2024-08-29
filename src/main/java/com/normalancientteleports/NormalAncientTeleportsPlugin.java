af073dfcb4ec288a38bec3169b5796022e56d935
Zoltan Toth <88989745+zoltander@users.noreply.github.com>
2024-08-29 00:02:33 -0400

    spellbook specific name replacement redone

--- src/main/java/com/normalancientteleports/NormalAncientTeleportsPlugin.java
+++ src/main/java/com/normalancientteleports/NormalAncientTeleportsPlugin.java
@@ -4,10 +4,11 @@ import com.google.gson.Gson;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.Client;
+import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetID;
+import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
@@ -20,7 +21,8 @@ import java.util.List;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Normal Ancient Teleports"
+	name = "Normal Ancient Teleports",
+		description = "Renames Ancient Teleports to their actual locations."
 )
 public class NormalAncientTeleportsPlugin extends Plugin
 {
@@ -28,6 +30,9 @@ public class NormalAncientTeleportsPlugin extends Plugin
 
 	@Inject
 	private Client client;
+
+	@Inject
+	private ClientThread clientThread;
 
 	@Inject
 	private NormalAncientTeleportsConfig config;
@@ -48,6 +53,38 @@ public class NormalAncientTeleportsPlugin extends Plugin
 	@Override
 	protected void shutDown() throws Exception
 	{
+	}
+
+	@Subscribe
+	public void onScriptPreFired(ScriptPreFired event) {
+		if (event.getScriptId() != ScriptID.MAGIC_SPELLBOOK_INITIALISESPELLS) {
+			return;
+		}
+		int[] stack = client.getIntStack();
+		int sz = client.getIntStackSize();
+		int spellBookEnum = stack[sz - 12]; // eg 1982, 5285, 1983, 1984, 1985
+		clientThread.invokeLater(() -> renameSpells(spellBookEnum));
+
+	}
+
+	private void renameSpells(int spellBookEnum) {
+		EnumComposition spellbook = client.getEnum(spellBookEnum);
+		for (int i = 0; i < spellbook.size(); ++i) {
+			int spellObjId = spellbook.getIntValue(i);
+			ItemComposition spellObj = client.getItemDefinition(spellObjId);
+			int spellComponent = spellObj.getIntValue(ParamID.SPELL_BUTTON);
+			Widget w = client.getWidget(spellComponent);
+
+			w.setName(
+					w.getName().replace("Senntisten", "Exam Centre")
+							.replace("Paddewwa", "Edgeville Dungeon")
+							.replace("Kharyrll", "Canifis")
+							.replace("Lassar", "Ice Mountain")
+							.replace("Dareeyak", "Crazy Archaeologist")
+							.replace("Carrallanger", "Graveyard of Shadows")
+							.replace("Annakarl", "Demonic Ruins")
+							.replace("Ghorrock", "Frozen Waste"));
+		}
 	}
 
 	@Subscribe
